d93f0ff2a8715a156d5bcc29cc3ddabc62aa0e15
Gjworks <gjworks@kakao.com>
2023-11-20 16:24:01 +0900

    ddd

--- app/(pages)/maintenance/page.tsx
+++ app/(pages)/maintenance/page.tsx
@@ -1,57 +1,9 @@
 'use client'
 
-import Sectionbox from '@gjworks/components/sections/Sectionbox'
+import Coming from '@gjworks/components/message/Coming'
 
 const Page = () => {
-  return (
-    <div className="">
-      <div className="max-w-screen-xl mx-auto py-8 xl:py-20 px-3">
-        <div className="text-black dark:text-white text-5xl font-semibold">
-          Maintenance
-        </div>
-        <div className=" py-10">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="col-span-2 lg:col-span-1">
-              <Sectionbox>
-                <div className="py-10 px-5">
-                  <div className="text-white font-bold text-xl mb-5">
-                    홈페이지 유지보수
-                  </div>
-                </div>
-              </Sectionbox>
-            </div>
-            <div className="col-span-2 lg:col-span-1">
-              <Sectionbox>
-                <div className="py-10 px-5">
-                  <div className="text-white font-bold text-xl mb-5">
-                    홈페이지 유지보수
-                  </div>
-                </div>
-              </Sectionbox>
-            </div>
-            <div className="col-span-2 lg:col-span-1">
-              <Sectionbox>
-                <div className="py-10 px-5">
-                  <div className="text-white font-bold text-xl mb-5">
-                    홈페이지 유지보수
-                  </div>
-                </div>
-              </Sectionbox>
-            </div>
-            <div className="col-span-2 lg:col-span-1">
-              <Sectionbox>
-                <div className="py-10 px-5">
-                  <div className="text-white font-bold text-xl mb-5">
-                    홈페이지 유지보수
-                  </div>
-                </div>
-              </Sectionbox>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+  return <Coming></Coming>
 }
 
 export default Page
--- app/(pages)/works/page.tsx
+++ app/(pages)/works/page.tsx
@@ -1,53 +1,9 @@
 'use client'
 
-import React, { useState } from 'react'
-import { motion } from 'framer-motion'
+import Coming from '@gjworks/components/message/Coming'
 
-import Sectionbox from 'src/components/sections/Sectionbox'
-
-const Page = props => {
-  const parentVariants = {
-    onscreen: {
-      transition: { staggerChildren: 0.3 },
-    },
-    offscreen: {
-      transition: { staggerChildren: 0.3, staggerDirection: -1 },
-    },
-  }
-  const variants = {
-    onscreen: {
-      opacity: [0, 1],
-      transition: {
-        duration: 0.6,
-      },
-    },
-    offscreen: {
-      opacity: 0,
-    },
-  }
-  return (
-    <>
-      <div className="max-w-screen-xl mx-auto py-8 xl:py-20 px-3">
-        {/* <motion.div className="text-black dark:text-white text-5xl font-semibold mb-16">
-            Portfolio
-          </motion.div> */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
-          <Sectionbox
-            thumb="/assets/images/MackbookPro.png"
-            title="Web Metaverse를 VR로 즐겨보세요."
-            desc="Playcanvas를 활용하여 웹에서 VR HMD로 볼 수 있습니다. 언제 어디서든 이용 가능 합니다."
-            date="2023 / 02 / 28"
-          ></Sectionbox>
-          <Sectionbox
-            thumb="/assets/images/photo.jpg"
-            title="Playcanvas"
-            desc="Playcanvas를 활용하여 웹에서 접근이 가능한 메타버스 서비스를 구축 할 수 있게 도와드리겠습니다."
-            date="2023 / 02 / 28"
-          ></Sectionbox>
-        </div>
-      </div>
-    </>
-  )
+const Page = () => {
+  return <Coming></Coming>
 }
 
 export default Page
