9166a28c607c65b57985b77e7a560c8ba1b38aec
Gjworks <gjworks@kakao.com>
2024-02-14 10:18:24 +0900

    dd

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Page() {
                           className="mb-5 px-1  lg:mb-2"
                         >
                           <div className="flex flex-wrap gap-2 text-5xl font-semibold !leading-tight tracking-tighter text-black md:text-6xl lg:gap-0 dark:text-white">
-                            Building digital experience88
+                            Building digital experience
                           </div>
                         </motion.div>
 
--- src/components/message/Coming.tsx
+++ src/components/message/Coming.tsx
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 
 const Coming = () => {
   return (
-    <div className="relative w-full">
+    <div className="bg-dots relative w-full">
       {/* <div className="absolute inset-0 bg-gradient-to-b from-white/90 via-transpanrent to-white/90"></div> */}
       <div className="">
         {/* <div className="absolute inset-0 bg-gray-950/10"></div> */}
-        <div className="relative max-w-screen-xl mx-auto px-3 h-[calc(100vh-236px)] md:h-[calc(100vh-162px)]">
-          <div className="flex justify-center items-center w-full h-full">
+        <div className="relative mx-auto h-[calc(100vh-236px)] max-w-screen-xl px-3 md:h-[calc(100vh-162px)]">
+          <div className="flex h-full w-full items-center justify-center">
             <div className="text-2xl text-gray-900">Coming Soon</div>
           </div>
         </div>
@@ -21,6 +21,6 @@ const Coming = () => {
         className="rounded-2xl"
       /> */}
     </div>
-  )
-}
-export default Coming
+  );
+};
+export default Coming;
