b6ce4a8354ec2c56c414495cd142240b997014f3
Gjworks <gjworks@kakao.com>
2024-08-05 15:24:50 +0900

    dd

--- prisma/migrations/20240418025951_tables/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `moduleId` to the `Module` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Module` ADD COLUMN `moduleId` VARCHAR(191) NOT NULL;
