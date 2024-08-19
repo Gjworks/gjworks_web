804739ff814495137dcbd998b358558d058b5f23
Gjworks <gjworks@kakao.com>
2024-08-19 17:48:59 +0900

    supports

--- src/app/(pages)/supports/page.tsx
+++ src/app/(pages)/supports/page.tsx
@@ -1,106 +1,208 @@
 'use client'
 import { HomeIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
+
 const Page = () => {
   return (
     <>
-      <div className="max-w-screen-xl mx-auto px-3">
-        <div className="flex items-center pt-10 pb-16 gap-2">
-          <div className="text-gray-950 dark:text-white">
-            <HomeIcon className="size-3.5 stroke-1.5" />
+      <div className="relative w-full bg-[url('/assets/images/bg16.jpg')] bg-no-repeat bg-cover bg-center">
+        <div className="absolute top-0 left-0 bottom-0 right-0 overflow-hidden from-gray-950/50 via-gray-950/60 to-gray-950/70 bg-gradient-to-b dark:from-dark-950/70 dark:via-dark-950/70 dark:to-dark-950/70"></div>
+        <div className="relative max-w-screen-xl mx-auto px-3">
+          <div className="flex items-center pt-10 pb-16 gap-2">
+            <div className="text-gray-200 dark:text-white">
+              <HomeIcon className="size-3.5 stroke-1.5" />
+            </div>
+            <div className="text-gray-300 dark:text-white">
+              <ChevronRightIcon className="size-3.5 stroke-1.5" />
+            </div>
+            <div className="text-xs font-bold text-gray-100 dark:text-gray-600 uppercase">
+              Supports
+            </div>
           </div>
-          <div className="text-gray-700 dark:text-white">
-            <ChevronRightIcon className="size-3.5 stroke-1.5" />
+        </div>
+        <div className="relative max-w-screen-xl mx-auto px-3 flex items-center gap-2 lg:gap-4 justify-center lg:justify-start flex-wrap pb-16">
+          <div className="text-3xl font-bold w-full lg:w-auto text-center lg:text-left text-white">
+            고객센터
           </div>
-          <div className="text-xs font-bold text-gray-400 dark:text-gray-600 uppercase">
-            Supports
+          <div className="text-base text-gray-400 w-full lg:w-auto text-center lg:text-left">
+            개발관련 정보 공유
+          </div>
+        </div>
+        <div className="relative max-w-screen-xl mx-auto px-3 pb-12">
+          <div className="grid grid-cols-3 gap-8">
+            <div className="col-span-1">
+              <div className="bg-white/95 backdrop-blur-lg rounded-lg shadow-md shadow-gray-950/10 overflow-hidden">
+                <div className="p-3">
+                  <div className="font-semibold text-base mb-3">
+                    개발관련 질문을 하려면?
+                  </div>
+                  <div className="flex mb-8">
+                    <div className="bg-gray-950 text-white px-3 py-0.5 rounded-md text-sm">
+                      답변대기중
+                    </div>
+                  </div>
+                  <div className="flex gap-2 flex-wrap">
+                    <div className="text-purple-900 bg-purple-200 py-0.5 px-2 rounded-md text-sm">
+                      react.js
+                    </div>
+                    <div className="text-orange-900 bg-orange-100 py-0.5 px-2 rounded-md text-sm">
+                      prisma.js
+                    </div>
+                  </div>
+                </div>
+                <div className="px-3 pb-3 border-t border-gray-300 bg-gray-200 pt-3">
+                  <div className="text-sm underline">
+                    프론트엔드 게시판 가기
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-1">
+              <div className="bg-white/95 backdrop-blur-lg rounded-lg shadow-md shadow-gray-950/10 overflow-hidden">
+                <div className="p-3">
+                  <div className="font-semibold text-base mb-3">
+                    개발관련 질문을 하려면?
+                  </div>
+                  <div className="flex mb-8">
+                    <div className="bg-gray-950 text-white px-3 py-0.5 rounded-md text-sm">
+                      답변대기중
+                    </div>
+                  </div>
+                  <div className="flex gap-2 flex-wrap">
+                    <div className="text-purple-900 bg-purple-200 py-0.5 px-2 rounded-md text-sm">
+                      react.js
+                    </div>
+                    <div className="text-orange-900 bg-orange-100 py-0.5 px-2 rounded-md text-sm">
+                      prisma.js
+                    </div>
+                  </div>
+                </div>
+                <div className="px-3 pb-3 border-t border-gray-300 bg-gray-200 pt-3">
+                  <div className="text-sm underline">
+                    프론트엔드 게시판 가기
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-1">
+              <div className="bg-white/95 backdrop-blur-lg rounded-lg shadow-md shadow-gray-950/10 overflow-hidden">
+                <div className="p-3">
+                  <div className="font-semibold text-base mb-3">
+                    개발관련 질문을 하려면?
+                  </div>
+                  <div className="flex mb-8">
+                    <div className="bg-gray-950 text-white px-3 py-0.5 rounded-md text-sm">
+                      답변대기중
+                    </div>
+                  </div>
+                  <div className="flex gap-2 flex-wrap">
+                    <div className="text-purple-900 bg-purple-200 py-0.5 px-2 rounded-md text-sm">
+                      react.js
+                    </div>
+                    <div className="text-orange-900 bg-orange-100 py-0.5 px-2 rounded-md text-sm">
+                      prisma.js
+                    </div>
+                  </div>
+                </div>
+                <div className="px-3 pb-3 border-t border-gray-300 bg-gray-200 pt-3">
+                  <div className="text-sm underline">
+                    프론트엔드 게시판 가기
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white border-b border-gray-200">
+        <div className="max-w-screen-xl mx-auto px-3">
+          <div className="flex items-center h-24 w-3/5 mx-auto gap-8">
+            <div className="text-lg font-semibold text-black dark:text-white">
+              Search
+            </div>
+            <div className="border-b border-gray-300 h-8 flex-1 hover:border-gray-950"></div>
           </div>
         </div>
       </div>
       <div className="bg-gray-100 dark:bg-dark-950">
-        <div className="p-2 lg:p-5">
-          <div className="relative mx-auto max-w-screen-md px-3 lg:px-8 bg-white dark:bg-dark-900 border border-gray-200 dark:border-dark-700 shadow-sm shadow-gray-300 dark:shadow-black rounded-xl">
-            <div className="py-8">
-              <div className="grid grid-cols-2 gap-8">
-                <div className="lg:order-0 order-1 col-span-2">
-                  <div className="grid grid-cols-4 gap-8 py-10">
-                    <div className="col-span-4 lg:col-span-1">
-                      <div className="text-lg font-semibold text-black dark:text-white">
-                        BUSINESS INFO
+        <div className="max-w-screen-xl mx-auto px-3">
+          <div className="grid grid-cols-2 gap-8">
+            <div className="lg:order-0 order-1 col-span-2">
+              <div className="grid grid-cols-4 gap-8 py-10">
+                <div className="col-span-4 lg:col-span-1">
+                  <div className="text-lg font-semibold text-black dark:text-white">
+                    BUSINESS INFO
+                  </div>
+                </div>
+                <div className="col-span-4 lg:col-span-3">
+                  <div className="mb-10">
+                    <div className="mb-2 text-base">
+                      <div className="text-dark-300 mb-3 mr-3">
+                        gjworks2@gmail.com
                       </div>
-                    </div>
-                    <div className="col-span-4 lg:col-span-3">
-                      <div className="mb-10">
-                        <div className="mb-2 text-base">
-                          <div className="text-dark-300 mb-3 mr-3">
-                            gjworks2@gmail.com
-                          </div>
-                          <div className="flex flex-wrap">
-                            <div className="flex w-full gap-2 lg:w-auto">
-                              <span className="text-dark-400">
-                                사업자등록번호:
-                              </span>
-                              <span className="mr-3 text-black dark:text-white">
-                                892-28-00572
-                              </span>
-                            </div>
-                            <div className="flex w-full gap-2 lg:w-auto">
-                              <span className="text-dark-400">대표:</span>
-                              <span className="text-black dark:text-white">
-                                김규진
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-span-4 lg:col-span-1">
-                      <div className="text-lg font-semibold text-black dark:text-white">
-                        소셜네트워크
-                      </div>
-                    </div>
-                    <div className="col-span-4 lg:col-span-3">
-                      <div className="mb-10">
-                        <div className="mb-4 text-base font-semibold text-black dark:text-white">
-                          오픈채팅
-                        </div>
-                        <div className="mb-2 flex items-center text-sm">
-                          <div className="pr-2">
-                            <span className="inline-block rounded-full bg-amber-400 px-5 py-1 text-center text-black">
-                              카카오톡
-                            </span>
-                          </div>
-                          <div className="flex-1 pl-3 ">
-                            <span className="text-dark-400">
-                              카카오톡 오픈채팅 문의를 가실려면 클릭해주세요.
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="mb-10">
-                        <div className="mb-4 flex flex-wrap items-center text-base font-semibold text-black">
-                          <div className="mr-2 w-full text-black dark:text-white lg:w-auto">
-                            소셜링크
-                          </div>
-                          <div className="text-dark-400 w-full text-sm lg:w-auto">
-                            주요 소셜 네트워크 링크입니다.
-                          </div>
-                        </div>
-                        <div className="mb-2 flex items-center text-sm">
-                          <span className="mr-2 inline-block rounded-full bg-blue-700 px-5 py-1 text-center text-white">
-                            페이스북
+                      <div className="flex flex-wrap">
+                        <div className="flex w-full gap-2 lg:w-auto">
+                          <span className="text-dark-400">사업자등록번호:</span>
+                          <span className="mr-3 text-black dark:text-white">
+                            892-28-00572
                           </span>
-                          <div className="flex-1 pl-3 ">
-                            <div className="text-dark-500">@gjworks2</div>
-                          </div>
                         </div>
-                        <div className="mb-2 flex items-center text-sm">
-                          <span className="mr-2 inline-block rounded-full bg-sky-500 px-5 py-1 text-center text-white">
-                            트위터
+                        <div className="flex w-full gap-2 lg:w-auto">
+                          <span className="text-dark-400">대표:</span>
+                          <span className="text-black dark:text-white">
+                            김규진
                           </span>
-                          <div className="flex-1 pl-3 ">
-                            <span className="text-dark-500">@gjworks2</span>
-                          </div>
                         </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-4 lg:col-span-1">
+                  <div className="text-lg font-semibold text-black dark:text-white">
+                    소셜네트워크
+                  </div>
+                </div>
+                <div className="col-span-4 lg:col-span-3">
+                  <div className="mb-10">
+                    <div className="mb-4 text-base font-semibold text-black dark:text-white">
+                      오픈채팅
+                    </div>
+                    <div className="mb-2 flex items-center text-sm">
+                      <div className="pr-2">
+                        <span className="inline-block rounded-full bg-amber-400 px-5 py-1 text-center text-black">
+                          카카오톡
+                        </span>
+                      </div>
+                      <div className="flex-1 pl-3 ">
+                        <span className="text-dark-400">
+                          카카오톡 오픈채팅 문의를 가실려면 클릭해주세요.
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mb-10">
+                    <div className="mb-4 flex flex-wrap items-center text-base font-semibold text-black">
+                      <div className="mr-2 w-full text-black dark:text-white lg:w-auto">
+                        소셜링크
+                      </div>
+                      <div className="text-dark-400 w-full text-sm lg:w-auto">
+                        주요 소셜 네트워크 링크입니다.
+                      </div>
+                    </div>
+                    <div className="mb-2 flex items-center text-sm">
+                      <span className="mr-2 inline-block rounded-full bg-blue-700 px-5 py-1 text-center text-white">
+                        페이스북
+                      </span>
+                      <div className="flex-1 pl-3 ">
+                        <div className="text-dark-500">@gjworks2</div>
+                      </div>
+                    </div>
+                    <div className="mb-2 flex items-center text-sm">
+                      <span className="mr-2 inline-block rounded-full bg-sky-500 px-5 py-1 text-center text-white">
+                        트위터
+                      </span>
+                      <div className="flex-1 pl-3 ">
+                        <span className="text-dark-500">@gjworks2</span>
                       </div>
                     </div>
                   </div>
--- src/layouts/fullLayout/Header.tsx
+++ src/layouts/fullLayout/Header.tsx
@@ -142,17 +142,17 @@ const Header = () => {
               </button>
               <div className="relative flex items-center">
                 <div className="col-span-1 relative flex justify-center items-center gap-1">
-                  <div className="group relative flex items-center">
+                  <div className="group relative flex items-center gap-2">
                     <Link
                       href="/"
-                      className="flex items-center justify-center bg-dark-950 rounded-md p-0.5"
+                      className="flex items-center justify-center bg-dark-950 rounded-md p-[1.5px]"
                     >
                       <Image
                         src="/assets/images/brand/gjworks_white.svg"
                         alt="gjworks logo"
                         width="32"
                         height="32"
-                        className="block w-[1.6rem] h-[1.6rem] lg:w-[2.1rem] lg:h-[2.1rem]"
+                        className="block w-[1.6rem] h-[1.6rem] lg:w-[2rem] lg:h-[2rem]"
                       />
                       {/* <Image
                         src="/assets/images/brand/gjworks.svg"
@@ -162,21 +162,41 @@ const Header = () => {
                         className="block dark:hidden lg:h-10 lg:w-10 h-8 w-8"
                       /> */}
                     </Link>
-                    <div className="hidden group-hover:flex absolute top-0.5 lg:top-2 -right-10 z-10">
-                      <div className="bg-pink-400 text-white py-1 px-2 rounded-md text-xs after:w-3 after:h-3 after:rounded-sm after:absolute after:-left-1 after:top-1.5 after:bg-pink-400 after:inline-block after:rotate-45 after:content-[''] after:z-50">
+                    <div className="hidden group-hover:flex absolute top-10 lg:top-12 -left-0.5 z-10">
+                      <div className="bg-pink-400 text-white py-1 px-3 rounded-md text-xs after:w-3 after:h-3 after:rounded-sm after:absolute after:left-3.5 after:-top-1 after:bg-pink-400 after:inline-block after:rotate-45 after:content-[''] z-10 after:z-[-1]">
                         2.0
                       </div>
                     </div>
                     <a
                       href={process.env.NEXT_PUBLIC_DEFAULT_URL}
-                      className="hidden items-center"
+                      className="hidden lg:flex items-center"
                     >
-                      <div className="mr-4 flex pl-2 text-sm font-medium lg:text-sm">
+                      <div className="mr-4 flex pl-2 text-sm font-bold lg:text-base">
                         <div className="text-black dark:text-white">
-                          gjworks
+                          지제이웍스
                         </div>
                       </div>
                     </a>
+                    <Link
+                      href="/plextype"
+                      className="hidden dark:text-dark-100 dark:bg-dark-100/10 cursor-pointer rounded-full bg-gray-200/90 px-4 py-1 text-xs text-gray-500 backdrop-blur-lg transition duration-300 hover:bg-gray-200/75 hover:text-black dark:hover:bg-gray-100/20 dark:hover:text-white"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1}
+                        stroke="currentColor"
+                        className="mr-2 h-4 w-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46"
+                        />
+                      </svg>
+                      <span>Plextype 0.1.10.beta</span>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -322,26 +342,6 @@ const Header = () => {
                   </svg>
                 </button>
               </div>
-              <Link
-                href="/plextype"
-                className="hidden dark:text-dark-100 dark:bg-dark-100/10 cursor-pointer rounded-full bg-gray-100/90 px-4 py-1 text-xs text-gray-500 backdrop-blur-lg transition duration-300 hover:bg-gray-200/75 hover:text-black dark:hover:bg-gray-100/20 dark:hover:text-white"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1}
-                  stroke="currentColor"
-                  className="mr-2 h-4 w-4"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46"
-                  />
-                </svg>
-                <span>Plextype 0.1.10.beta</span>
-              </Link>
             </div>
             <div className="relative col-span-1 flex items-center justify-end gap-2 lg:gap-4 lg:px-3">
               <div className="flex items-center">
@@ -350,7 +350,7 @@ const Header = () => {
                   // onClick={() => setShowModal(!showModal)}
                   onClick={() => setShowRight(true)}
                 >
-                  <BellIcon className="size-6 stroke-1.5" />
+                  <BellIcon className="size-6 stroke-1" />
                 </button>
                 <div className="lg:pr-3">
                   <AccountDropwdown />
