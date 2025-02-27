08dc27b06af1f7aa4fb02d9f664e7b5ab7221a05
Gjworks <gjworks@kakao.com>
2025-02-27 11:10:08 +0900

    Signin

--- src/app/api/auth/Signin/route.ts
+++ src/app/api/auth/Signin/route.ts
@@ -1,30 +1,32 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 import { cookies, headers } from "next/headers";
 import { hashedPassword, verifyPassword } from "@plextype/utils/auth/password";
-import { decodeJwt } from 'jose';
-import { sign, verify, refresh, refreshVerify } from "@plextype/utils/auth/jwtAuth";
+import { decodeJwt } from "jose";
+import {
+  sign,
+  verify,
+  refresh,
+  refreshVerify,
+} from "@plextype/utils/auth/jwtAuth";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
-  const cookieStore = await cookies()
-  const formData = await request.formData(); 
-  console.log(formData.get('accountId')); 
-
+  const cookieStore = await cookies();
+  const formData = await request.formData();
 
   const accountId = formData.get("accountId") as string;
   const password = formData.get("password") as string;
 
   if (!accountId) {
-
     const response = {
       success: true,
       type: "error",
       element: "accountId",
       message: "계정 아이디 (혹은 이메일)을 입력해주세요.",
-      data : {}
-    }
+      data: {},
+    };
     console.log(response);
     return NextResponse.json(response);
   }
@@ -34,8 +36,8 @@ export async function POST(request: Request) {
       type: "error",
       element: "password",
       message: "비밀번호를 입력해주세요.",
-      data : {}
-    }
+      data: {},
+    };
     return NextResponse.json(response);
   }
 
@@ -44,28 +46,33 @@ export async function POST(request: Request) {
       where: { accountId: accountId },
     });
 
+    console.log(userInfo);
+    const vpass = await verifyPassword(password, userInfo!.password);
+    console.log(vpass);
+
     if (userInfo! && (await verifyPassword(password, userInfo!.password))) {
       // exclude password from json response
       const tokenParams = {
         id: userInfo!.id,
-        accountId:userInfo!.accountId,
-        isAdmin:userInfo!.isAdmin
-      }
-      
-      let refreshToken
-      let accessToken
-      [accessToken, refreshToken] = await Promise.all([sign(tokenParams), refresh(tokenParams)])
-      console.log(accessToken)
-      const response = 
-        {
-          success: true,
-          type: "success",
-          data: {
-            userInfo: userInfo
-          },
-          accessToken: accessToken,
-        }
+        accountId: userInfo!.accountId,
+        isAdmin: userInfo!.isAdmin,
+      };
 
+      let refreshToken;
+      let accessToken;
+      [accessToken, refreshToken] = await Promise.all([
+        sign(tokenParams),
+        refresh(tokenParams),
+      ]);
+      console.log(accessToken);
+      const response = {
+        success: true,
+        type: "success",
+        data: {
+          userInfo: userInfo,
+        },
+        accessToken: accessToken,
+      };
 
       cookieStore.set({
         name: "accessToken",
@@ -86,17 +93,17 @@ export async function POST(request: Request) {
 
       return NextResponse.json(response);
     } else {
-      const response = 
-        {
-          success: false,
-          type: "error",
-          message : '아이디 혹은 비밀번호가 맞지 않거나 존재 하지 않은 계정입니다.',
-          data: {},
-          accessToken:null
-        }
+      console.log(111111111);
+      const response = {
+        success: false,
+        type: "error",
+        message:
+          "아이디 혹은 비밀번호가 맞지 않거나 존재 하지 않은 계정입니다.",
+        data: {},
+        accessToken: null,
+      };
       return NextResponse.json(response);
     }
-
   } catch (e) {
     console.error(e);
     const response = {
@@ -108,7 +115,4 @@ export async function POST(request: Request) {
     };
     return NextResponse.json(response);
   }
-
-
-  
 }
