90bf97160801eebfda42d0ff33899e1d18c03463
Gjworks <gjworks@kakao.com>
2025-03-13 12:00:56 +0900

    build error

--- next.config.js
+++ next.config.js
@@ -12,8 +12,5 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone", // PM2 실행 시 필요
-  experimental: {
-    serverActions: {}, // 필요한 경우 유지, 아니면 제거 가능
-  },
 };
 module.exports = nextConfig;
