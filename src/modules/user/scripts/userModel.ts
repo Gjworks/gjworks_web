dfc931085a89ce90af351e642d8dfb7184408101
Gjworks <gjworks@kakao.com>
2025-03-13 11:43:05 +0900

    Updated userModel and added configuration files

--- src/modules/user/scripts/userModel.ts
+++ src/modules/user/scripts/userModel.ts
@@ -1,25 +1,25 @@
-'use server';
+"use server";
 
 import { cookies, headers } from "next/headers";
 import { NextResponse } from "next/server";
-import { decodeJwt } from 'jose';
+import { decodeJwt } from "jose";
 import { PrismaClient } from "@prisma/client";
 
 interface UserListParams {
-  page: number | null
-  keyword: string | null
-  target: string | null
+  page: number | null;
+  keyword: string | null;
+  target: string | null;
 }
 
 interface UserParams {
-  id? : number | null
-  uuid? : string | null
+  id?: number | null;
+  uuid?: string | null;
 }
 
 interface LoggedParams {
-  id: number
-  accountId : string
-  isAdmin? : boolean
+  id: number;
+  accountId: string;
+  isAdmin?: boolean;
 }
 
 interface UserSessionResponse {
@@ -32,30 +32,30 @@ interface UserSessionResponse {
 const prisma = new PrismaClient();
 
 export const getUserLogged = async () => {
-  const cookieStore = await cookies()
-  const accessToken = cookieStore.get('accessToken')?.value
-  let tokenInfo:LoggedParams = {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  let tokenInfo: LoggedParams = {
     id: 0,
-    accountId : '',
-    isAdmin : false
+    accountId: "",
+    isAdmin: false,
+  };
+  if (accessToken) {
+    const decodeToken: { id: number; accountId: string; isAdmin: boolean } =
+      await decodeJwt(accessToken);
+    tokenInfo.accountId = decodeToken.accountId;
+    tokenInfo.id = decodeToken.id;
+    tokenInfo.isAdmin = decodeToken.isAdmin;
   }
-  if(accessToken) {
-    const decodeToken:{ id:number, accountId:string, isAdmin:boolean } = await decodeJwt(accessToken);
-    tokenInfo.accountId = decodeToken.accountId
-    tokenInfo.id = decodeToken.id
-    tokenInfo.isAdmin = decodeToken.isAdmin
-  }
-  return tokenInfo
-}
+  return tokenInfo;
+};
 
 export const getUserSession = async (): Promise<UserSessionResponse> => {
-  const tokenInfo = await getUserLogged()
+  const tokenInfo = await getUserLogged();
 
   let userInfo: UserInfo | null = null;
-  let response :UserSessionResponse
-  console.log('tokenInfo', tokenInfo)
-  if(tokenInfo) {
-
+  let response: UserSessionResponse;
+  console.log("tokenInfo", tokenInfo);
+  if (tokenInfo) {
     try {
       const userInfo = await prisma.user.findUnique({
         where: { accountId: tokenInfo.accountId },
@@ -75,89 +75,90 @@ export const getUserSession = async (): Promise<UserSessionResponse> => {
           },
         },
       });
-      console.log(userInfo)
+      console.log(userInfo);
       response = {
         success: true,
-        data: userInfo
-      }
-      
+        data: userInfo,
+      };
     } catch (e) {
-      console.log('getUser error' + e)
+      console.log("getUser error" + e);
       response = {
         success: true,
         type: "error",
-        message : '회원정보가 없습니다.',
-        data: null
+        message: "회원정보가 없습니다.",
+        data: null,
       };
     }
-  }else {
+  } else {
     response = {
       success: true,
       type: "error",
-      message : '토큰 정보가 올바르지 않습니다.',
-      data: null
+      message: "토큰 정보가 올바르지 않습니다.",
+      data: null,
     };
   }
 
-  console.log('response', response)
-  return response
-}
+  console.log("response", response);
+  return response;
+};
 
-export const getUser = async (params:UserParams) => {
-  const cookieStore = await cookies()
-  const accessToken = cookieStore.get('accessToken')?.value
+export const getUser = async (params: UserParams) => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
   let obj: any = {};
-  let loggedInfo:LoggedParams = {
+  let loggedInfo: LoggedParams = {
     id: 0,
-    accountId : ''
+    accountId: "",
+  };
+  let userInfo: any = null;
+  let response = {};
+  if (accessToken) {
+    const decodeToken: { id: number; accountid: string; isAdmin: boolean } =
+      await decodeJwt(accessToken);
+    loggedInfo.accountId = decodeToken.accountid;
   }
-  let userInfo = {} || null
-  let response = {}
-  if(accessToken) {
-    const decodeToken:{ id:number, accountid:string, isAdmin:boolean } = await decodeJwt(accessToken);
-    loggedInfo.accountId = decodeToken.accountid
-  }
-  params.id && (obj.id = params.id)
-  params.uuid && (obj.uuid = params.uuid).trim()
+  params.id && (obj.id = params.id);
+  params.uuid && (obj.uuid = params.uuid).trim();
 
-  if(!obj.id && !obj.uuid && !obj.nickname && !obj.accountId) {
-    obj.accountId = loggedInfo.accountId
+  if (!obj.id && !obj.uuid && !obj.nickname && !obj.accountId) {
+    obj.accountId = loggedInfo.accountId;
   }
-  if(!obj) {
-    return response = {
+  if (!obj) {
+    return (response = {
       success: true,
       type: "error",
-      message : '데이터가 없습니다.',
-      data: {}
-    };
+      message: "데이터가 없습니다.",
+      data: {},
+    });
   }
   try {
-    if(obj.id || obj.uuid) {
+    if (obj.id || obj.uuid) {
       userInfo = await prisma.user.findUnique({
         where: obj,
         include: {
           userGroups: {
             include: {
-              group: true
+              group: true,
             },
           },
         },
-      })
+      });
     }
   } catch (e) {
-    console.log('getUser error' + e)
-    response = {
-      success: true,
+    console.log("getUser error:", e);
+    return {
+      success: false,
       type: "error",
-      message : '회원정보가 없습니다.',
-      data: {}
+      message: "회원정보가 없습니다.",
+      data: {},
     };
   }
-  response = userInfo || {}
-  return response
-}
+  return (
+    userInfo || { success: false, message: "회원정보가 없습니다.", data: {} }
+  );
+};
 
-export const getUserList = async (params:UserListParams) => {
+export const getUserList = async (params: UserListParams) => {
   const { page, target, keyword } = params;
 
   const currentPage = page; // 현재 페이지
@@ -177,111 +178,113 @@ export const getUserList = async (params:UserListParams) => {
     take: listCount,
     where,
     orderBy: {
-      id: 'desc',
+      id: "desc",
     },
   });
   const returnData = {
-    userList : userList,
-    navigation : {
-      totalCount : totalItems,
-      totalPages : totalPages,
-      page : currentPage,
-      listCount : listCount
-    }
-  }
+    userList: userList,
+    navigation: {
+      totalCount: totalItems,
+      totalPages: totalPages,
+      page: currentPage,
+      listCount: listCount,
+    },
+  };
   // const result = await userList?.json()
-  return returnData
-}
+  return returnData;
+};
 
-export const getUserByAccountId = async (accountId:string) => {
-  let userInfo
-  let response
+export const getUserByAccountId = async (accountId: string) => {
+  let userInfo;
+  let response;
   try {
-    console.log(accountId)
+    console.log(accountId);
     userInfo = await prisma.user.findUnique({
-      where: { accountId: accountId }
+      where: { accountId: accountId },
     });
   } catch (e) {
-    console.log('getUser error' + e)
+    console.log("getUser error" + e);
     response = {
       success: true,
       type: "error",
-      message : '회원정보가 없습니다.',
-      data: {}
+      message: "회원정보가 없습니다.",
+      data: {},
     };
   }
-  response = userInfo
-  return response
-}
+  response = userInfo;
+  return response;
+};
 
-export const getUserByNickname = async (nickName:string) => {
-
-  let userInfo
-  let response
+export const getUserByNickname = async (nickName: string) => {
+  let userInfo;
+  let response;
   try {
     userInfo = await prisma.user.findUnique({
       where: { nickName: nickName },
     });
   } catch (e) {
-    console.log('getUser error' + e)
-    response ={
+    console.log("getUser error" + e);
+    response = {
       success: true,
       code: "error",
-      message : '회원정보가 없습니다.',
-      data: {}
+      message: "회원정보가 없습니다.",
+      data: {},
     };
   }
-  response = userInfo
-  return response
-}
+  response = userInfo;
+  return response;
+};
 
 export const getUserByToken = async (token) => {
-  let userInfo
-  let response
-  const cookieStore = await cookies()
-  const accessToken = cookieStore.get('accessToken')?.value
-  
+  let userInfo;
+  let response;
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+
   if (!accessToken) {
-    response =  NextResponse.json({
-      success: true,
-      type: "error",
-        message : 'accessToken Error',
-      data: {}
-    },
-    {
-      status: 200,
-    },);
-  } else {
-    const decodeToken:{ id:string, accountId:string, isAdmin:boolean } = await decodeJwt(accessToken);
-    if (!decodeToken || !decodeToken.id) {
-      response = NextResponse.json({
+    response = NextResponse.json(
+      {
         success: true,
         type: "error",
-        message: 'Invalid accessToken',
-        data: {}
-      }, {
+        message: "accessToken Error",
+        data: {},
+      },
+      {
+        status: 200,
+      },
+    );
+  } else {
+    const decodeToken: { id: string; accountId: string; isAdmin: boolean } =
+      await decodeJwt(accessToken);
+    if (!decodeToken || !decodeToken.id) {
+      response = NextResponse.json(
+        {
+          success: true,
+          type: "error",
+          message: "Invalid accessToken",
+          data: {},
+        },
+        {
           status: 200,
-      });
+        },
+      );
     } else {
       userInfo = await prisma.user.findUnique({
         where: { accountId: decodeToken.accountId },
       });
 
-      if(!userInfo) {
+      if (!userInfo) {
+        response = NextResponse.json({
+          success: true,
+          type: "error",
+          message: "회원정보가 없습니다.",
+          data: {},
+        });
+      } else {
         response = NextResponse.json(
           {
             success: true,
-            type: "error",
-            message : '회원정보가 없습니다.',
-            data: {}
-          }
-        );
-      }else{
-
-        response = NextResponse.json(
-          {
-            success: true,
-            data: userInfo
+            data: userInfo,
           },
           {
             status: 200,
@@ -293,7 +296,7 @@ export const getUserByToken = async (token) => {
   response = NextResponse.json(
     {
       success: true,
-      data: userInfo
+      data: userInfo,
     },
     {
       status: 200,
@@ -301,6 +304,5 @@ export const getUserByToken = async (token) => {
   );
   // const nowPasswordValue = request.get('nowPasswordValue') as string;
   // console.log(nowPasswordValue)
-  return response
-}
-
+  return response;
+};
