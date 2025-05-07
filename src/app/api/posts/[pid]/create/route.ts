f55c3fe3f8813e73aea662e21e52120942eb7872
Gjworks <gjworks@kakao.com>
2025-05-07 16:25:30 +0900

    권한을 위한 서버 컴포넌트 구조 변경

--- src/app/api/posts/[pid]/create/route.ts
+++ src/app/api/posts/[pid]/create/route.ts
@@ -38,7 +38,7 @@ export async function GET(request: Request, segmentData: { params: Params }) {
 }
 
 export async function POST(request: Request, segmentData: { params: Params }) {
-  // let response = {};
+  let response = {};
   // const { pid } = await segmentData.params;
   // const postData = await request.json();
   //
